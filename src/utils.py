fa71539cdc8b7d30d2a0cdeffcc0ce8b7f0a3591
irustam <49302555+irustam@users.noreply.github.com>
2020-09-11 19:50:25 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,21 +7,21 @@ def prefilter_items(data, take_n_popular=5000, popular_col='quantity', min_value
     data2 = data.copy()
     
     # 1. Удаление товаров, со средней ценой < 1$
-#     data2.loc[data2['sales_value']/data2['quantity'] < min_value, 'item_id'] = pseudo_item_id
+    data2.loc[data2['sales_value']/data2['quantity'] < min_value, 'item_id'] = pseudo_item_id
 #     data2 = data2.loc[data2['sales_value']/data2['quantity'] > min_value]
     
     # 2. Удаление товаров со соедней ценой > 30$
-#     data2.loc[data2['sales_value']/data2['quantity'] > max_value, 'item_id'] = pseudo_item_id
+    data2.loc[data2['sales_value']/data2['quantity'] > max_value, 'item_id'] = pseudo_item_id
 #     data2 = data2.loc[data2['sales_value']/data2['quantity'] < max_value]
     
     # 3. Придумайте свой фильтр
     # 3.1 Убираем непонятную положительную скидку
-#     data2.loc[data2['retail_disc'] > 0, 'item_id'] = pseudo_item_id
+    data2.loc[data2['retail_disc'] > 0, 'item_id'] = pseudo_item_id
 #     data2 = data2.loc[data2['retail_disc'] <= 0]
     
     # 3.2 Оставляем только товары, купленные в последние N недель:
-#     from_week = data2['week_no'].max() - week_left
-#     data2 = data2.loc[data2['week_no'] > from_week]
+    from_week = data2['week_no'].max() - week_left
+    data2 = data2.loc[data2['week_no'] > from_week]
     
     # 4. Выбор топ-N самых популярных товаров (N = take_n_popular)    
     popularity = data2.groupby('item_id')[popular_col].sum().reset_index()
